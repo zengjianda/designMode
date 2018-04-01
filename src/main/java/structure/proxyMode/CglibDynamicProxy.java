2b97a0811c5bf994d5aaaabb1d06bab8bcea123f
zengjianlu <zengjianlu@360jk.com>
2018-04-01 17:15:09 +0800

    cglib动态代理

--- src/main/java/structure/proxyMode/CglibDynamicProxy.java
+++ src/main/java/structure/proxyMode/CglibDynamicProxy.java
@@ -1,4 +1,24 @@
 package structure.proxyMode;
 
-public class CglibDynamicProxy implements M{
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+public class CglibDynamicProxy implements MethodInterceptor{
+
+    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+        before();
+        Object result = methodProxy.invokeSuper(o,objects);
+        after();
+        return result;
+    }
+
+    public void before(){
+        System.out.println("before method");
+    }
+
+    public void after(){
+        System.out.println("after method");
+    }
 }
--- src/main/java/structure/proxyMode/Test.java
+++ src/main/java/structure/proxyMode/Test.java
@@ -1,13 +1,23 @@
 package structure.proxyMode;
 
+import net.sf.cglib.proxy.Enhancer;
+
 /**
  * Created by zengjianlu on 2018/2/23.
  */
 public class Test {
     public static void main(String[] args) {
+        //静态代理
 //        Sourceable sourceable = new Proxy();
 //        sourceable.method();
-        Sourceable sourceable = (Sourceable) new DynamicProxy().getProxyObject(new Source());
+        //jdk代理
+//        Sourceable sourceable = (Sourceable) new DynamicProxy().getProxyObject(new Source());
+//        sourceable.method();
+        //cglib代理
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(Source.class);
+        enhancer.setCallback(new CglibDynamicProxy());
+        Sourceable sourceable = (Sourceable) enhancer.create();
         sourceable.method();
     }
 }
